7ba1bba9169c4bdc184ac9ceb73ed70f9ec3ec8c
chkumar <himakiran@gmail.com>
2020-09-06 16:18:11 +0530

    Imported helium

--- MyAmazonSeleniumApp/main.py
+++ MyAmazonSeleniumApp/main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from helium import *
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def open_a_window(url):
+    start_chrome(url)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    open_a_window('https://www.amazon.in/login')
